f229929978b4aa7ccc1658c8bbb6ac7b2b025ef3
Simas Sliacinas <simas@Simass-MacBook-Pro-2.local>
2025-03-29 19:38:45 +0000

    Fixing clodbuild trigger

--- deployment/terraform/cloudbuild.tf
+++ deployment/terraform/cloudbuild.tf
@@ -1,6 +1,13 @@
+resource "google_service_account" "cloudbuild_sa" {
+  account_id   = "cloudbuild-service-acc"
+  display_name = "Cloud Build Service Account"
+  project      = var.project_id
+}
+
 resource "google_cloudbuild_trigger" "merge_main" {
   name = "merge-main"
   filename = "../cloudbuild.yaml"
+  service_account = google_service_account.cloudbuild_sa.id
   github {
     owner = "simas-s"
     name = "SDE-Tech-Task-ThnXSL4"
