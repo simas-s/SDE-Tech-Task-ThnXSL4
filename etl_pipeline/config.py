cdc5877059f8723acb39823139f99ddce5eef01d
Simas Sliacinas <simas@Simass-MacBook-Pro-2.local>
2025-03-30 17:17:48 +0100

    Null default state_code

--- etl_pipeline/config.py
+++ etl_pipeline/config.py
@@ -4,13 +4,13 @@ from dataclasses import dataclass
 @dataclass
 class City:
     city_name: str
-    state_code: str
+    state_code: str = None
     country_code: str = "US"
 
-# TODO: Handling missing state code
+
 TARGET_CITIES = {
-    City(city_name="Sioux Falls", state_code=),
-    City(city_name="Great Falls", state_code=),
+    City(city_name="Sioux Falls"),
+    City(city_name="Great Falls"),
     City(city_name="Houghton", state_code="Michigan"),
     City(city_name="Fargo", state_code="North Dakota"),
     City(city_name="Duluth", state_code="Minnesota"),
@@ -18,6 +18,6 @@ TARGET_CITIES = {
     City(city_name="Aberdeen", state_code="South Dakota"),
     City(city_name="Grand Island", state_code="Nebraska"),
     City(city_name="Glasgow", state_code="Montana"),
-    City(city_name="Omaha", state_code=),
-    City(city_name="Portland", state_code=)
+    City(city_name="Omaha"),
+    City(city_name="Portland")
 }
